fe76a9925ec806a7ceb13e43e8fac58a2e8430f8
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-21 11:52:52 +0530

    UC1-Create a simple linked list

---
+++ LinedListMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.linkedlist;
+
+public class LinedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(56);
+        linkedList.add(30);
+        linkedList.display();
+    }
+
+}
